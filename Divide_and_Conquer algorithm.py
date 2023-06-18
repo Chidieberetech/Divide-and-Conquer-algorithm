85328f75c76e7a8a28991f6849bb7da137f4d53e
Teroxlab <emmanuels820@gmail.com>
2023-06-18 15:06:11 +0200

    Divide and Conquer - UPDATED

--- Divide_and_Conquer algorithm.py
+++ Divide_and_Conquer algorithm.py
@@ -1,12 +1,10 @@
 import math
 
-
 def distance(point1, point2):
     """
     Helper function to calculate the Euclidean distance between two points.
     """
-    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
 def brute_force(points):
     """
@@ -15,8 +13,9 @@ def brute_force(points):
     min_distance = float('inf')
     closest_points = ()
 
-    for i in range(len(points)):
-        for j in range(i + 1, len(points)):
+    n = len(points)
+    for i in range(n):
+        for j in range(i + 1, n):
             dist = distance(points[i], points[j])
             if dist < min_distance:
                 min_distance = dist
@@ -24,6 +23,21 @@ def brute_force(points):
 
     return min_distance, closest_points
 
+def strip_closest(strip_points, min_distance, closest_pair):
+    """
+    Helper function to find the closest pair of points within the strip.
+    """
+    n = len(strip_points)
+    for i in range(n):
+        j = i + 1
+        while j < n and strip_points[j][1] - strip_points[i][1] < min_distance:
+            dist = distance(strip_points[i], strip_points[j])
+            if dist < min_distance:
+                min_distance = dist
+                closest_pair = (strip_points[i], strip_points[j])
+            j += 1
+
+    return min_distance, closest_pair
 
 def closest_pair(points):
     """
@@ -60,25 +74,10 @@ def closest_pair(points):
     strip_points_sorted_by_y = sorted(strip_points, key=lambda point: point[1])
 
     # Check for closer pairs within the strip
-    strip_min_dist = min_dist
-    strip_closest_pair = closest_pair
-
-    for i in range(len(strip_points_sorted_by_y)):
-        j = i + 1
-        while j < len(strip_points_sorted_by_y) and strip_points_sorted_by_y[j][1] - strip_points_sorted_by_y[i][
-            1] < strip_min_dist:
-            dist = distance(strip_points_sorted_by_y[i], strip_points_sorted_by_y[j])
-            if dist < strip_min_dist:
-                strip_min_dist = dist
-                strip_closest_pair = (strip_points_sorted_by_y[i], strip_points_sorted_by_y[j])
-            j += 1
+    min_dist, closest_pair = strip_closest(strip_points_sorted_by_y, min_dist, closest_pair)
 
     # Return the closest pair and its distance
-    if strip_min_dist < min_dist:
-        return strip_min_dist, strip_closest_pair
-    else:
-        return min_dist, closest_pair
-
+    return min_dist, closest_pair
 
 # Test the algorithm with a sample set of points
 points = [(1, 2), (5, 6), (3, 4), (9, 8), (2, 10)]
